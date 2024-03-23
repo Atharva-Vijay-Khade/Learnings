191cf2ab17a4db3c98eef60db6b19b0516d36fc6
Atharva Vijay Khade <atharvavk123@gmail.com>
2024-03-23 19:47:24 +0530

    create card and learn about modifiers and other properties

--- Android-Development/Android-Jetpack-Compose-Bootcamp/app/src/main/java/com/example/bizcard/MainActivity.kt
+++ Android-Development/Android-Jetpack-Compose-Bootcamp/app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +25,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BizCardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                CreateBizCard()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateBizCard() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp)
+                        ,shape = RoundedCornerShape(corner = CornerSize(15.dp))
+                        ,elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                        ,colors = CardDefaults.cardColors(containerColor = Color.White)){
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
