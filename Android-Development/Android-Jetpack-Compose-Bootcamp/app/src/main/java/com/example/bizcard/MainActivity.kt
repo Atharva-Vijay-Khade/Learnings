3bd7f3ba0d1ba20000f449000c9799ec55b8e2b3
Atharva Vijay Khade <atharvavk123@gmail.com>
2024-03-23 20:27:25 +0530

    Add profile image

--- Android-Development/Android-Jetpack-Compose-Bootcamp/app/src/main/java/com/example/bizcard/MainActivity.kt
+++ Android-Development/Android-Jetpack-Compose-Bootcamp/app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -3,19 +3,27 @@ package com.example.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizCardTheme
@@ -43,7 +51,18 @@ fun CreateBizCard() {
                         ,shape = RoundedCornerShape(corner = CornerSize(15.dp))
                         ,elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
                         ,colors = CardDefaults.cardColors(containerColor = Color.White)){
-
+                Surface(modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp)
+                    .align(Alignment.CenterHorizontally),
+                    shape = CircleShape,
+                    color = Color.White,
+                    border = BorderStroke(0.5.dp, Color.Black),
+                    shadowElevation = 10.dp) {
+                        Image(painter = painterResource(id = R.drawable.profile_image),
+                            contentDescription = "profile image",
+                            contentScale = ContentScale.Crop)
+                }
         }
     }
 }
